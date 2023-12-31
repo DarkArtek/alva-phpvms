43b1669ff8e6e42e81c7da696056177d9d89bf94
Taylor Broad <taylorbroad@gmail.com>
2023-12-31 13:45:01 +0900

    Modified deployment script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,12 +3,13 @@ set -e
 
 echo "Deployment started ..."
 
+su spark
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin production
+git pull spk production
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
