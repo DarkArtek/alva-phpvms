fbab0b22a1b52c438cc2d7b99b53cea8f887053c
Taylor Broad <taylorbroad@gmail.com>
2024-02-13 23:43:57 +0900

    Add ACARS Download Links

--- resources/views/layouts/spark24/downloads/index.blade.php
+++ resources/views/layouts/spark24/downloads/index.blade.php
@@ -4,6 +4,24 @@
 @section('content')
   @include('flash::message')
   <div class="row">
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-header">Approved ACARS Clients</div>
+        <div class="card-body">
+          <div class="d-flex flex-row justify-content-around">
+            <div>
+              <a class="btn btn-lg btn-primary" href="https://smartcars.tfdidesign.com/central/download">smartCARS 3</a>
+            </div>
+            <div>
+              <a class="btn btn-lg btn-primary" href="https://storage.googleapis.com/storage.flyspark.org/ACARS.zip">vmsACARS</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
     <div class="row-sm-12">
       <h2>{{ trans_choice('common.download', 2) }}</h2>
     </div>
