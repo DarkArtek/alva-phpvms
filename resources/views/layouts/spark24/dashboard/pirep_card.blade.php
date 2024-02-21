ee90464f83695163b2ff2fae8c51561f556bbbcf
Taylor Broad <taylorbroad@gmail.com>
2024-02-22 00:38:52 +0900

    Fix Html. Fix it more later.

--- resources/views/layouts/spark24/dashboard/pirep_card.blade.php
+++ resources/views/layouts/spark24/dashboard/pirep_card.blade.php
@@ -1,22 +1,20 @@
 <div class="card-body" style="min-height: 0px">
   <div class="row">
-    <div class="col-sm-10">
+    <div class="col-sm-9">
       <p>
         <a href="{{ route('frontend.pireps.show', [$pirep->id]) }}">{{ $pirep->ident }}</a>
         -
-        {{ $pirep->dpt_airport->name }}
-        (<a href="{{route('frontend.airports.show', [
+        <a href="{{route('frontend.airports.show', [
                           'id' => $pirep->dpt_airport->icao
-                          ])}}">{{$pirep->dpt_airport->icao}}</a>)
+                          ])}}">{{$pirep->dpt_airport->icao}}</a>
         <span class="description">to</span>
-        {{ $pirep->arr_airport->name }}
-        (<a href="{{route('frontend.airports.show', [
+        <a href="{{route('frontend.airports.show', [
                           'id' => $pirep->arr_airport->icao
-                          ])}}">{{$pirep->arr_airport->icao}}</a>)
+                          ])}}">{{$pirep->arr_airport->icao}}</a>
       </p>
     </div>
-    <div class="col-sm-2 float-right">
-      <div class="col-sm-2 text-center">
+    <div class="col-sm-3 float-right">
+      <div class="text-center">
           @if($pirep->state === PirepState::PENDING)
             <div class="badge badge-warning">
           @elseif($pirep->state === PirepState::ACCEPTED)
@@ -27,9 +25,11 @@
              <div class="badge badge-info">
           @endif
             {{ PirepState::label($pirep->state) }}</div>
-          <a href="{{ route('frontend.pireps.edit', [$pirep->id]) }}"
-            class="btn btn-sm btn-info">@lang('common.edit')</a> 
-      </div>    
+
+      </div>
+
     </div>
+                <a href="{{ route('frontend.pireps.edit', [$pirep->id]) }}"
+                   class="btn btn-sm btn-block btn-info">@lang('common.edit')</a>
   </div>
 </div>
